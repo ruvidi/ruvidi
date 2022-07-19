52494e4607e9a5b8d16356b918bb6dc6c2409aa7
msowho <msokto@yandex.ru>
2022-07-20 01:34:47 +0500

    tries to do transcoder class with upload

--- blueprints.py
+++ blueprints.py
@@ -1,8 +1,34 @@
-from flask import Blueprint, request, render_template
+from flask import abort, request, Blueprint, render_template
+from database import database
+from models import Video
 
 index_blueprint = Blueprint("index_blueprint", "index_blueprint", url_prefix='/')
 
 
 @index_blueprint.get('/')
-def hello():
+def index():
     return render_template("index.html")
+
+
+@index_blueprint.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        if not 'title' and 'description' in request.form or not 'video' in request.files:
+            return abort(400)
+        
+        video = Video(request.form['title'], request.form['description'])
+
+        database.session.add(video)
+        database.session.commit()
+
+        return render_template("upload_status.html", video=video)
+    return render_template("upload.html")
+
+@index_blueprint.get('/upload_status/<int:id>')
+def upload_status(id: int):
+    video = Video.query.get(id)
+
+    if not video:
+        return abort(404)
+    
+    return render_template("upload_status.html", video=video)
--- models.py
+++ models.py
@@ -3,4 +3,17 @@ from database import database
 
 class Video(database.Model):
     id = database.Column(database.Integer, primary_key=True, autoincrement=True)
+    title = database.Column(database.String(100))
+    description = database.Column(database.String(500), nullable=True)
+    source_url = database.Column(database.String(100), nullable=True)
+    om_url = database.Column(database.String(100), nullable=True) # for old mobiles (low 3gp format)
+    public = database.Column(database.Boolean)
+    views = database.Column(database.Integer)
+
+    def __init__(self, title, description, public=False) -> None:
+        self.title = title
+        self.description = description
+        self.public = public
     
+    def __repr__(self) -> str:
+        return '<Video {0}>'.format(self.title[:17] + '...' if len(self.title) > 20 else self.title)
